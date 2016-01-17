3e5e526639c4a7d88bd4a83c18d58a296a2fccf7
Darren Oakley <daz.oakley@gmail.com>
2016-01-17 14:31:09 +0000

    Refactor NotamBuilder to reduce duplication.

--- app/domain/notam_builder.rb
+++ app/domain/notam_builder.rb
@@ -16,29 +16,15 @@ class NotamBuilder
     end
 
     def extract_hours_of_service(str)
-      str = hours_of_service_extract_str(str)
-
+      str  = hours_of_service_extract_str(str)
       hos  = {}
       dow  = %w(MON TUE WED THU FRI SAT SUN)
       dows = %i(monday tuesday wednesday thursday friday saturday sunday)
 
-      # single day
-      dow.each_with_index do |day, index|
-        match = str.match(/#{day} ?([\d+\s-]+)/)
-        next unless match
-
-        time_str = match[1].chomp.strip
-        next if time_str.empty?
-
-        hos[dows[index]] = time_str
-      end
-
-      # multi-day
       dow.each_with_index do |start_day, start_index|
         dow.each_with_index do |end_day, end_index|
-          next if start_day == end_day
-
-          match = str.match(/#{start_day}-#{end_day} ?([\d+\s-]+)/)
+          days  = start_day == end_day ? start_day : "#{start_day}-#{end_day}"
+          match = str.match(/#{days} ?([\d+\s-]+)/)
           next unless match
 
           time_str = match[1].chomp.strip
